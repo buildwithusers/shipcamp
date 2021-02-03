951b4f4730f46dea1fda11510c50cb978382a9c3
Steven <steven@grible.co>
2021-02-03 15:16:23 +0100

    Setup raw page layout with codepen embed

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,64 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container mx-auto my-8">
+    <h1>Build a weather app using Tailwind &amp; Vue.js</h1>
+    <header class="flex space-x-2">
+      <figure
+        class="preview border-2 border-gray-100 rounded-md w-2/3 flex items-center justify-center h-48"
+      >
+        Preview comes here
+      </figure>
+      <aside
+        class="border-2 border-gray-100 rounded-md w-1/3 px-4 py-4 flex items-center"
+      >
+        <img
+          src="https://pbs.twimg.com/profile_images/1309864829931450378/-hM7wXiI_400x400.jpg"
+          alt=""
+          class="rounded-full w-12 mr-2"
+        />
+        <h3 class="font-bold">Dude die het maakt</h3>
+      </aside>
+    </header>
+
+    <header class="summary my-4 gap-2 grid grid-cols-3 text-left">
+      <div class="card bg-blue-100 rounded-md p-4">
+        <h3 class="font-bold mb-1">Summary</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur,
+          corporis enim asperiores laudantium explicabo dolore veritatis debitis
+          ipsum odio saepe!
+        </p>
+      </div>
+      <div class="card bg-blue-100 rounded-md p-4">
+        <h3 class="font-bold mb-1">What you'll learn</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione,
+          aliquam!
+        </p>
+      </div>
+      <div class="card bg-blue-100 rounded-md p-4">
+        <h3 class="font-bold mb-1">What we use</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit
+          consequuntur recusandae atque.
+        </p>
+      </div>
+    </header>
+
+    <hr class="my-8" />
+
+    <CodePen />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CodePen from "./components/CodePenExample.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    CodePen,
+  },
+};
 </script>
 
 <style>
@@ -23,6 +68,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/CodePenExample.vue
@@ -0,0 +1,22 @@
+<template>
+  <iframe
+    height="265"
+    style="width: 100%;"
+    scrolling="no"
+    title="Task Management UI"
+    src="https://codepen.io/knyttneve/embed/vYEzXOR?height=265&theme-id=light&default-tab=html,result"
+    frameborder="no"
+    loading="lazy"
+    allowtransparency="true"
+    allowfullscreen="true"
+  >
+    See the Pen
+    <a href="https://codepen.io/knyttneve/pen/vYEzXOR">Task Management UI</a> by
+    Mert Cukuren (<a href="https://codepen.io/knyttneve">@knyttneve</a>) on
+    <a href="https://codepen.io">CodePen</a>.
+  </iframe>
+</template>
+
+<script>
+export default {};
+</script>
