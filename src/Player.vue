8daf96737e724b13085c2a49d8ad888f13630c83
Steven <steven@grible.co>
2021-02-17 13:44:54 +0100

    Add play/pause button

--- src/Player.vue
+++ src/Player.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <figure id="player-container" class="bg-black" @click="handleClick">
+    <figure id="player-container" class="bg-black" @click="togglePlay">
       <video
         ref="vid"
         class=""
@@ -11,6 +11,37 @@
         <source :src="currentSrc" />
       </video>
     </figure>
+    <div id="controls" class="bg-gray-900 h-7 flex align-middle px-4">
+      <button id="play-pause" @click="togglePlay">
+        <svg
+          v-if="!playing"
+          class="text-gray-100 w-5 h-5 hover:text-gray-300"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z"
+            clip-rule="evenodd"
+          />
+        </svg>
+
+        <svg
+          v-else
+          class="text-gray-100 w-5 h-5 hover:text-gray-300"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zM7 8a1 1 0 012 0v4a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v4a1 1 0 102 0V8a1 1 0 00-1-1z"
+            clip-rule="evenodd"
+          />
+        </svg>
+      </button>
+    </div>
 
     <hr />
     <h2>debug</h2>
@@ -55,7 +86,7 @@ export default Vue.extend({
     },
   },
   methods: {
-    handleClick(e) {
+    togglePlay() {
       if (this.playing) {
         this.$refs.vid.pause();
         return;
