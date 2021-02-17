b6c60a70b2885111da301da09b5f75bafcffb819
Steven <steven@grible.co>
2021-02-17 11:58:00 +0100

    Add simple player with 1 clip

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,19 @@
     <h1>Build a weather app using Tailwind &amp; Vue.js</h1>
 
     <header class="flex space-x-2">
-      <figure
-        class="preview border-2 border-gray-100 w-full h-screen rounded-md flex items-center justify-center"
-      >
-        Player comes here
-      </figure>
+      <Player
+        class="preview border-2 border-gray-100 w-full h-screen rounded-md"
+      />
     </header>
   </div>
 </template>
 
 <script>
+import Player from "./Player.vue";
+
 export default {
   name: "App",
-  components: {},
+  components: { Player },
 };
 </script>
 
---
+++ src/Player.vue
@@ -0,0 +1,38 @@
+<template>
+  <figure>
+    <video src="scenes/sample-scene-forrest.mp4"></video>
+  </figure>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  data() {
+    return {
+      txt: "Here comes the player",
+      $video: null,
+    };
+  },
+  mounted() {
+    this.$video = this.$el.getElementsByTagName("video")[0];
+    this.$video.play();
+  },
+});
+</script>
+
+<style>
+::-webkit-media-controls {
+  display: none !important;
+}
+video::-webkit-media-controls {
+  display: none !important;
+}
+video::-webkit-media-controls-enclosure {
+  display: none !important;
+}
+
+video:focus {
+  outline: none;
+}
+</style>
