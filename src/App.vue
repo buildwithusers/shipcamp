2bc3a697c2b2d0c8dc682429e3ef6e5fdfa40d8f
Gino Mangnoesing <gino@grible.co>
2021-02-17 13:53:07 +0100

    add re-usable scene component

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,23 @@
 <template>
   <div id="app" class="container mx-auto my-8">
-    <h1>Build a weather app using Tailwind &amp; Vue.js</h1>
-
+    <h1>Build a Unsplash-powered weather app</h1>
     <header class="flex space-x-2">
       <Player
         class="preview border-2 border-gray-100 w-full h-screen rounded-md"
       />
     </header>
+    <br>
+    <Scene><p>Interactive things.</p></Scene>
   </div>
 </template>
 
 <script>
 import Player from "./Player.vue";
+import Scene from './components/Scene';
 
 export default {
   name: "App",
-  components: { Player },
+  components: { Player, Scene},
 };
 </script>
 
---
+++ src/components/Scene.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="self">
+    <slot></slot>
+  </div>
+</template>
+<script>
+import Vue from 'vue';
+
+export default Vue.component('Scene', {});
+</script>
+<style scoped>
+  .self {
+    width: 896px;
+    height: 560px;
+    background-color: #2E3541;
+    color: white;
+    display: flex;
+    align-items: center;
+    justify-self: center;
+  }
+</style>
